6807c8bf123d3f92bacda8a768937b327f0bbbc9
ayushv-nitj <ayush7610jsr@gmail.com>
2025-12-13 15:37:28 +0530

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold">
+        MemoryLane 🌱
+      </h1>
+      <br /> 
+      <h2>
+         Digital Time Capsule Platform
+      </h2>
+    </main>
+  );
+}
